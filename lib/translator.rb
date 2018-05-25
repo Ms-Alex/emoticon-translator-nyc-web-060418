40570d63f0a6b4e3c415d591948daabee4e8f2de
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-25 01:53:06 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,8 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   hash = load_library(file_path)
   
-  hash.values[0].each do |gets_keys|
+  hash.values[0].each do |emo, meaning|
+    if emo == 
     gets
   
   binding.pry
