874a3ec373ca9752f8901e1bdc67df047eea058f
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-25 01:48:56 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
-  emo = YAML.load_file(file_path)
+  load_library(file_path)
   
+  return 
 end
 
 def get_english_meaning
